a16792125f8b5a2f2078ea241c105832c57e18d8
Adel Qalieh <aqalieh95@gmail.com>
2013-07-22 22:29:18 -0400

    Read index.html from disk

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  f = fs.readFileSync('index.html');
+  stringbuffer = buf.toString(f);
+
+  response.send(stringbuffer);
 });
 
 var port = process.env.PORT || 5000;
